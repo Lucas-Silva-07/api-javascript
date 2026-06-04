4898502ec79a9ccbd58f358a5742c797a605c9d4
Lucas <lucasalves_07@outlook.com>
2026-06-03 21:11:38 -0300

    Feito endpoint de criação de nova linha

--- server.js
+++ server.js
@@ -9,17 +9,18 @@ app.post('/clients', async (req, res) => {
   try { 
     await prisma.clients.create({
       data: {
-        name:    req.body.name,
-        phone:   req.body.phone,
-        date:    req.body.date, 
-        hour:    req.body.hour,
-        price:   req.body.price,
-        service: req.body.service
+        name:     req.body.name,
+        phone:    req.body.phone,
+        date: new Date(req.body.date), 
+        hour:     req.body.hour,
+        price:    req.body.price,
+        service:  req.body.service
       }
     });
     res.status(201).json(req.body);
     } catch (error) {
-      res.status(500).json({error: 'Erro ao criar'});
+      console.log(error)
+      res.status(400).json({error: 'Erro ao criar'});
     }
   })
 
@@ -35,5 +36,20 @@ app.get('/clients', async (req, res) => {
     res.status(500).json({ error: 'Erro ao buscar clientes' });
   }
 }); 
+
+// Listar horario de acordo com a data
+app.get('/scheduleDate', async (req, res) => {
+  try {
+    const hours = await prisma.clients.findMany({
+      where: {
+        date: req.body.date
+      }
+    });
+    res.status(400).json(req.body);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({error: 'Erro na busca dos horarios'})
+  }
+})
 
 app.listen(3000);
