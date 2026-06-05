37695cddee7b7ea544aa8321ed0b39b576bb5bf8
Lucas <lucasalves_07@outlook.com>
2026-06-05 09:51:56 -0300

    Ajustando rotas de editar e deletar Feito os testes e funcionando perfeitamente

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/clients', async (req, res) => {
     const clients = await prisma.clients.findMany();
     console.log(clients);
 
-    res.json(clients);
+    res.status(200).json(clients);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Erro ao buscar clientes' });
@@ -45,10 +45,48 @@ app.get('/scheduleAt', async (req, res) => {  // {"date": "2026-06-03"}
         date: new Date(req.body.date)
       }
     });
-    res.status(400).json(hours.map(item => item.hour));
+    res.status(200).json(hours.map(item => item.hour));
   } catch (error) {
     console.error(error);
     res.status(500).json({error: 'Erro na busca dos horarios'})
+  }
+});
+
+// Editar Agendamento
+app.put('/clients/:id', async (req, res) => {
+  try{
+    await prisma.clients.update({
+      where: {
+        id: Number(req.params.id)
+      },
+      data: {
+        name:     req.body.name,
+        phone:    req.body.phone,
+        date: new Date(req.body.date), 
+        hour:     req.body.hour,
+        price:    req.body.price,
+        service:  req.body.service
+      }
+    });
+    res.status(200).json(req.body)
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({error: 'ID não encontrado'})
+  }
+});
+
+// Deletar Agendamento por telefone
+app.delete('/clients/:id', async (req, res) => {
+  try{
+    await prisma.clients.delete({
+      where: {
+        id: Number(req.params.id)
+      }
+    })
+    res.status(200).json({Successfully: 'Agendamento deletado'})
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({error: 'ID não encontrado'})
   }
 });
 
